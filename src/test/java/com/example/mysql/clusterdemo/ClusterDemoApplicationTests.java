8a4fe6335fe6e65e5913b58ef21c926c245e8d0d
joe zhang <joe.zhang@ringcentral.com>
2020-07-18 22:54:25 +0800

    init project

---
+++ src/test/java/com/example/mysql/clusterdemo/ClusterDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysql.clusterdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClusterDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
